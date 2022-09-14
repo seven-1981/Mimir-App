a822f6a217f6440f6f6074b4f1512a2459c7750a
MBOS <michael.bos@ost.ch>
2022-09-14 21:28:50 +0200

    Redo to a stage that works

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { CardsScreen } from "./CardsScreen";
+import { NewGamePage } from "./NewGamePage";
 
 const ROUTE_HOME = "/";
 const ROUTE_CARDS = "/cards";
+const ROUTE_NEW_GAME = "/game";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Routes>
         <Route path={ROUTE_HOME} element={<Home />}>
           <Route path={ROUTE_CARDS} element={<CardsScreen />} />
+          <Route path={ROUTE_NEW_GAME} element={<NewGamePage />} />
         </Route>
       </Routes>
     </>
--- src/TitleBar.tsx
+++ src/TitleBar.tsx
@@ -4,8 +4,8 @@ export const TitleBar = () => {
   return (
     <div>
       Mimir
-      <Link to={"/game"}>New Game</Link>
-      <Link to={"/cards"}>Manage Cards</Link>
+      <Link to={"/game"}>New Game </Link>
+      <Link to={"/cards"}> Manage Cards</Link>
     </div>
   );
 };
