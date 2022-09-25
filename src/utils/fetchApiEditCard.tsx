076c43d37d54b4215835cfd107709134b6d746a0
Daniel Schlatter <daniel.schlatter@ost.ch>
2022-09-25 12:22:16 +0200

    Findings from Tobi done
    - fetchDelete and fetchEdit do not use cards, removed from fetch body

--- src/utils/fetchApiEditCard.tsx
+++ src/utils/fetchApiEditCard.tsx
@@ -1,16 +1,12 @@
-import { Card } from "../models/Card";
-
 export async function fetchApiEditCard(
   URL: string,
-  id: string,
-  newCard: Card
+  id: string
 ): Promise<boolean> {
   const urlWithId = URL + "/" + id;
   try {
     const apiResponse = await fetch(urlWithId, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newCard),
     });
     return apiResponse.ok;
   } catch (error) {
