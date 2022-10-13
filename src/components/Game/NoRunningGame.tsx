d6f35a8a114173f1e8a96bc3e1f6ddd2f00742e1
Daniel Schlatter <daniel.schlatter@ost.ch>
2022-10-13 20:56:42 +0200

    Simplified Running / NoRunningGame

--- src/components/Game/NoRunningGame.tsx
+++ src/components/Game/NoRunningGame.tsx
@@ -1,16 +1,56 @@
-import { StyledButton, StyledInputForm, StyledLabel } from "../styles";
+import {
+  StyledButton,
+  StyledGameResult,
+  StyledInputForm,
+  StyledLabel,
+} from "../styles";
+import { GameResultTable } from "./GameResultTable";
+import { emptyGame, Game } from "../../models/Game";
+import { fetchApiGetGame } from "../../utils/fetchApiGet";
+import { fetchApi, fetchApiWithData } from "../../utils/fetchApi";
+import { useContext } from "react";
+import { AppContext } from "../../store/context";
 
-export interface NoRunningGameProps {
-  onClickStartButton: () => void;
-}
+export const NoRunningGame = () => {
+  const { game, dispatch } = useContext(AppContext);
 
-export const NoRunningGame = (props: NoRunningGameProps) => {
-  return (
-    <StyledInputForm>
-      <StyledButton onClick={props.onClickStartButton}>
-        Start New Game
-      </StyledButton>
-      <StyledLabel>No game running</StyledLabel>
-    </StyledInputForm>
-  );
+  const gameFinished = game.solved.length === game.cardCount;
+
+  const onClickStartButton = async () => {
+    const { success: successGet } = await fetchApiGetGame("/api/game");
+    if (successGet) {
+      const successDelete = await fetchApi("/api/game", "DELETE");
+      if (!successDelete) {
+        return;
+      }
+    }
+    const { success: successPost, data: game } = await fetchApiWithData<
+      Game,
+      Game
+    >("/api/game", "POST", emptyGame);
+    if (!successPost || !game) {
+      return;
+    }
+    dispatch({ type: "update-game", game: game });
+  };
+
+  if (gameFinished) {
+    return (
+      <StyledGameResult>
+        <StyledButton onClick={() => onClickStartButton()}>
+          Start New Game
+        </StyledButton>
+        <GameResultTable solved={game.solved} />
+      </StyledGameResult>
+    );
+  } else {
+    return (
+      <StyledInputForm>
+        <StyledButton onClick={() => onClickStartButton()}>
+          Start New Game
+        </StyledButton>
+        <StyledLabel>No game running</StyledLabel>
+      </StyledInputForm>
+    );
+  }
 };
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,40 +1,18 @@
 import { RunningGame } from "../components/Game/RunningGame";
 import { NoRunningGame } from "../components/Game/NoRunningGame";
-import { GameResult } from "../components/Game/GameResult";
 import { useContext } from "react";
 import { AppContext } from "../store/context";
-import { fetchApiGetGame } from "../utils/fetchApiGet";
-import { fetchApi, fetchApiWithData } from "../utils/fetchApi";
-import { emptyGame, Game } from "../models/Game";
+import { emptyGame } from "../models/Game";
 
 export const GamePage = () => {
-  const { game, dispatch } = useContext(AppContext);
+  const { game } = useContext(AppContext);
 
-  const gameProgress = game.solved.length;
-  const gameCardCount = game.cardCount;
+  const noRunningGame =
+    game.cardCount === emptyGame.cardCount ||
+    game.solved.length === game.cardCount;
 
-  const onClickStartButton = async () => {
-    const { success: successGet } = await fetchApiGetGame("/api/game");
-    if (successGet) {
-      const successDelete = await fetchApi("/api/game", "DELETE");
-      if (!successDelete) {
-        return;
-      }
-    }
-    const { success: successPost, data: game } = await fetchApiWithData<
-      Game,
-      Game
-    >("/api/game", "POST", emptyGame);
-    if (!successPost || !game) {
-      return;
-    }
-    dispatch({ type: "update-game", game: game });
-  };
-
-  if (gameCardCount === emptyGame.cardCount) {
-    return <NoRunningGame onClickStartButton={() => onClickStartButton()} />;
-  } else if (gameProgress === gameCardCount) {
-    return <GameResult onClickStartButton={() => onClickStartButton()} />;
+  if (noRunningGame) {
+    return <NoRunningGame />;
   } else {
     return <RunningGame />;
   }
