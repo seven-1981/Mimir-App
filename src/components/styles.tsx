ab5cb1722d2b7184173fac07639b0288172905e0
MBOS <michael.bos@ost.ch>
2022-10-21 19:27:29 +0200

    added adaptive font size

--- src/components/styles.tsx
+++ src/components/styles.tsx
@@ -1,6 +1,7 @@
 import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
+import { adaptiveFontSize } from "../utils/adaptiveFontSize";
 
 const Background = css`
   min-height: 100vh;
@@ -20,7 +21,7 @@ export const GlobalStyle = createGlobalStyle`
 * {
     color: rgb(5, 6, 45);
     text-shadow: -1px 0 white, 0 1px black, 1px 0 black, 0 -1px white;
-    letter-spacing: 2px;
+    letter-spacing: 1px;
 }
 body {
 ${Background};
@@ -42,7 +43,7 @@ export const StyledTitleBar = styled.div`
 `;
 
 export const StyledLabel = styled.label`
-  font-size: 24px;
+  font-size: ${adaptiveFontSize(0.25, 0.5)};
 `;
 
 export const StyledCardListItem = styled.div`
@@ -50,7 +51,7 @@ export const StyledCardListItem = styled.div`
   justify-content: space-between;
   flex-align: left;
   grid-template-columns: 0.4fr 0.4fr 0.1fr 0.1fr;
-  gap: 10px;
+  gap: 12px;
   margin: 20px;
   padding: 5px;
   border: 1px solid grey;
@@ -63,13 +64,13 @@ const FancyButtonStyle = css`
   box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
   box-sizing: border-box;
   color: #ffffff;
-  font-size: 20px;
+  font-size: ${adaptiveFontSize(0.25, 0.5)};
   text-decoration: none;
   user-select: none;
   touch-action: manipulation;
   white-space: nowrap;
   cursor: pointer;
-  min-width: 120px;
+  min-width: 80px;
   min-height: 30px;
   padding: 10px;
 
@@ -93,7 +94,9 @@ export const StyledLink = styled(Link)`
 `;
 
 export const StyledInput = styled.input`
-  font-size: 1.2rem;
+  font-size: ${adaptiveFontSize(0.25, 0.5)};
+  min-width: 80px;
+  min-height: 30px;
   border-radius: 0.4rem;
   border: none;
   color: rgb(55,55,55);
@@ -139,21 +142,21 @@ export const StyledCardFront = styled.div`
   align-items: center;
   justify-content: center;
   margin: 20px;
-  width: 50vw;
+  width: 30vw;
   height: 30vh;
   color: rgb(5, 6, 45);
   background: white;
   border: solid;
   border-radius: 0.4rem;
   border-color: rgb(5, 6, 45);
-  font-size: calc(2.5vw + 0.5vh);
+  font-size: ${adaptiveFontSize(1, 1.5)};
 `;
 
 export const StyledResult = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-size: 1.5em;
+  font-size: ${adaptiveFontSize(0.5, 1)};
   margin: 50px;
 `;
 
---
+++ src/utils/adaptiveFontSize.tsx
@@ -0,0 +1,2 @@
+export const adaptiveFontSize = (height: number, width: number) =>
+  `calc(10px + ${height}vh + ${width}vw)`;
